23e8f593ce40eb6c9d923933881c93384d9358d5
bubbabaird <bubbabaird@me.com>
2017-05-31 13:28:56 -0400

    initial setup

---
+++ src/test/java/com/teeup/TeeupApplicationTests.java
@@ -0,0 +1,16 @@
+package com.teeup;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class TeeupApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
