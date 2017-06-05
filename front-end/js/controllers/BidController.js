a45f0066b627a6e06d7fe52956f2d7e248eecbfd
Colby Martin <colbymartin@Colbys-MacBook-Air.local>
2017-06-05 12:38:37 -0400

    Added Google Maps API

--- front-end/js/controllers/BidController.js
+++ front-end/js/controllers/BidController.js
@@ -1,6 +1,20 @@
 module.exports = {
     name: 'BidController',
     func: function ($scope, BidService) {
+        let input = document.getElementById('locationSearch');
+        let options = {
+            types: ['establishment']
+        };
+        let autocomplete = new google.maps.places.Autocomplete(input, options);
+        
+        autocomplete.addListener('place_changed', function () {
+            $scope.location = autocomplete.getPlace();
+            // $scope.location = {
+            //     lat: autocomplete.getPlace().location.geometry.location.lat(),
+            //     long: autocomplete.getPlace().location.geometry.location.long(),
+            // }
+        });
+
         $scope.bid_amount = '';
         $scope.star_select = '';
         $scope.start_time = '';
