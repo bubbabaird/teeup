2a848d8219477bb1a812b612456d5a31539de761
bcheeseman18 <benjamincheeseman18@gmail.com>
2017-06-05 10:30:11 -0400

    userboard changes

--- front-end/js/controllers/BidController.js
+++ front-end/js/controllers/BidController.js
@@ -2,7 +2,7 @@ module.exports = {
     name: 'BidController',
     func: function ($scope, BidService) {
         $scope.bid_amount = '';
-        $scope.submit() = function (bid_amount) {
+        $scope.submit = function (bid_amount) {
             BidService.submitBid($scope.bid_amount);
         }
         // $scope.result = BidService.getCourse(); 
